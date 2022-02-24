167f74f0239fdbca116d35f6c752260ca9bdf250
Paritosh-J <85030129+Paritosh-J@users.noreply.github.com>
2022-02-24 20:05:20 +0530

    Update handDetectionModule.py

--- handDetectionModule.py
+++ handDetectionModule.py
@@ -1,9 +1,10 @@
-'''
+"""
     Detect hands using cv2 & mediapipe library
     Draw lines to connect the landmarks of the detected hands
-'''
+"""
 import cv2
 import mediapipe as mp
+
 
 class handDetector():
     def __init__(self, mode=False, maxHands=2, detectConfidence=0.75, trackConfidence=0.75):
@@ -12,7 +13,8 @@ class handDetector():
         self.detectConfidence = detectConfidence
         self.trackConfidence = trackConfidence
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands() # process a RGB image & return the hand landmarks of detected hands
+        # process a RGB image & return the hand landmarks of detected hands
+        self.hands = self.mpHands.Hands()
         self.mpDraw = mp.solutions.drawing_utils
 
     def findHands(self, frame, draw=True):
@@ -22,39 +24,32 @@ class handDetector():
         if self.result.multi_hand_landmarks:
             for handLmarks in self.result.multi_hand_landmarks:
                 if draw:
-                    self.mpDraw.draw_landmarks(frame, handLmarks, self.mpHands.HAND_CONNECTIONS) # draw landmarks & connections for them
+                    # draw landmarks & connections for them
+                    self.mpDraw.draw_landmarks(
+                        frame, handLmarks, self.mpHands.HAND_CONNECTIONS)
         return frame
 
     def findPosition(self, frame, handNo=0, draw=True):
-        LmarkList = []
+        self.LmarkList = []
         rgbImg = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         self.result = self.hands.process(rgbImg)
+
         if self.result.multi_hand_landmarks:
-            myHand = self.result.multi_hand_landmarks[handNo] # processed landmarks from Hands() method for 1 hand
+            # processed landmarks from Hands() method for 1 hand
+            myHand = self.result.multi_hand_landmarks[handNo]
             # get land mark ids
             for LmarkId, lmk in enumerate(myHand.landmark):
                 h, w, c = frame.shape  # get o/p window dimension
-                # coordinates for landmakrs of detected hands
+                # coordinates for landmarks of detected hands
                 cx, cy = int(lmk.x * w), int(lmk.y * h)
-                print(LmarkId, cx, cy)  # print id & coord. of landmarks on console
-                LmarkList.append([LmarkId, cx, cy])
+                self.LmarkList.append([LmarkId, cx, cy])
 
                 if draw:
-                    if LmarkId == 4:  # id of thumb tip
-                        cv2.circle(frame, (cx, cy), 15, (84, 245, 66), cv2.FILLED)  # draw circle for thumb tip
-
-                    if LmarkId == 8:  # id of index finger tip
-                        cv2.circle(frame, (cx, cy), 15, (84, 245, 66), cv2.FILLED)  # draw circle for thumb tip
-
-                    if LmarkId == 12:  # id of middle finger tip
-                        cv2.circle(frame, (cx, cy), 15, (84, 245, 66), cv2.FILLED)  # draw circle for thumb tip
-
-                    if LmarkId == 16:  # id of ring finger tip
-                        cv2.circle(frame, (cx, cy), 15, (84, 245, 66), cv2.FILLED)  # draw circle for thumb tip
-
-                    if LmarkId == 20:  # id of pinky tip
-                        cv2.circle(frame, (cx, cy), 15, (84, 245, 66), cv2.FILLED)  # draw circle for thumb tip
-        return LmarkList
+                    if LmarkId in [4, 8, 12, 16, 20]:  # id of thumb tip
+                        # draw circle for thumb tip
+                        cv2.circle(frame, (cx, cy), 15,
+                                   (84, 245, 66), cv2.FILLED)
+        return self.LmarkList
 
 
 def main():
@@ -65,12 +60,16 @@ def main():
 
     while True:
         check, frame = video.read()  # check & capture the frame
-        frame = cv2.flip(frame, 1) # flip the frame for a mirror image like o/p
+        # flip the frame for a mirror image like o/p
+        frame = cv2.flip(frame, 1)
         frame = detect.findHands(frame)
-        LmarkList = detect.findPosition(frame) # get hand landmarks and store in a list
+        # get hand landmarks and store in a list
+        LmarkList = detect.findPosition(frame)
 
-        cv2.putText(frame, "Press 'Q' to exit", (25, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)  # display quit key on o/p window
-        cv2.imshow('=== Live Cam ===', frame) # open window for showing the o/p
+        cv2.putText(frame, "Press 'Q' to exit", (25, 450), cv2.FONT_HERSHEY_SIMPLEX,
+                    1, (255, 255, 0), 2)  # display quit key on o/p window
+        # open window for showing the o/p
+        cv2.imshow('=== Live Cam ===', frame)
 
         # escape key (q)
         if cv2.waitKey(1) == ord('q'):
